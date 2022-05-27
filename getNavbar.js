c17e3ebf329b3cf4fad5021375fd30c52f069d49
Nilesh Tiwari <100663297+Nilesh1409@users.noreply.github.com>
2022-05-27 10:44:23 +0530

    Add files via upload
    Component for navbar

---
+++ getNavbar.js
@@ -0,0 +1,34 @@
+let navBar =  function (){
+    return ` <div id="arrow">
+    <div>
+        <button id="leftArrow">
+            <svg role="img" height="24" width="24" class="Svg-sc-1bi12j5-0 jgfuCe IYDlXmBmmUKHveMzIPCF"
+                viewBox="0 0 24 24">
+                <path
+                    d="M15.957 2.793a1 1 0 010 1.414L8.164 12l7.793 7.793a1 1 0 11-1.414 1.414L5.336 12l9.207-9.207a1 1 0 011.414 0z">
+                </path>
+            </svg>
+        </button>
+    </div>
+    <div id="right-button">
+        <button id="rightArrow">
+            <svg style="color: white;" role="img" height="24" width="24"
+                class="Svg-sc-1bi12j5-0 jgfuCe IYDlXmBmmUKHveMzIPCF" viewBox="0 0 24 24">
+                <path style="color: white;"
+                    d="M8.043 2.793a1 1 0 000 1.414L15.836 12l-7.793 7.793a1 1 0 101.414 1.414L18.664 12 9.457 2.793a1 1 0 00-1.414 0z">
+                </path>
+            </svg>
+        </button>
+    </div>
+</div>
+<div id="sign-in">
+    <div id="log-in">
+        <button>Log in</button>
+    </div>
+    <div>
+        <button>Sign up</button>
+    </div>
+</div>`;
+}
+
+export default navBar;
