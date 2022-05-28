592bd60250fe359173b4d8ad8d352918d0febb33
azhar982111 <azhar982111@gmail.com>
2022-05-28 11:09:07 +0530

    pushing my branch

--- search.js
+++ search.js
@@ -1,6 +1,6 @@
 
 
-let accessToken = "BQCGmEqnY4rIUzr8v3n2t-q-59pr-qne5589StwJ0Sbj8CfUDfKMo0G-9js1-9NbwzkcVSpja9uqH8bhBcr-actc5LH7p52UdgCjUB3szd8o2w2lupI9iMCDA7u1pRfz5plaiwefFqFFqsUNU2O_CIP5yCjr1cjHCt4"
+let accessToken = "BQCWYZya-LkmBKs0BnaKVsws0SIwRn-qJWeLvnWqRNA-d94y8-bq_zDt2i3qJ0XSilFkZn0XjjZne5MwgO5fLkP0gipg6diMgAf7XNZd99l6mHUCZ30ns1IKYBidBkYne1UxfFqLgHZEkPdf8OgP6oVGU1Vsv9qmZgc"
 
 async function getdata(){
 try {
@@ -27,6 +27,26 @@ try {
 
 getdata()
 
+// search bar
+let timerId;
+        function debounce(storeInput,delay){
+            if(timerId) clearTimeout(timerId);
+        timerId=setTimeout(() => {
+            let input= document.querySelector("#inputSearch").value;
+    
+            storeInput(input);
+     
+        }, delay);
+        }
+    
+        function storeInput(input){
+            localStorage.setItem("searchInput",input);
+            
+
+            window.location.href="searchPage.html"
+        }
+
+
 function appendData(categories){
 
     let gridContainer = document.getElementById("gridContainer")
@@ -43,14 +63,15 @@ categories.forEach(element => {
     gridContainer.append(div)
 
     div.addEventListener("click", function(){
-        showPlaylist(element.id);
+        showPlaylist([element.id, element.name]);
     }, false);
 });
 
 }
 
-function showPlaylist(id){
-    localStorage.setItem("catID",id)
+function showPlaylist(cat){
+
+    localStorage.setItem("cat",cat)
     window.location.href = "http://127.0.0.1:5500/playlist.html"
 }
 
---
+++ upbar.js
@@ -0,0 +1,58 @@
+import navBar from "./getNavbar.js";
+console.log(navBar())
+document.getElementById("up-bar").innerHTML = navBar();
+
+
+
+let locationHref = JSON.parse(localStorage.getItem("locationHref")) || [];
+let loactionAt = locationHref.length - 1;
+console.log(locationHref)
+
+
+let forwardAt = JSON.parse(localStorage.getItem("forwardAt"));
+
+if(locationHref[forwardAt] == window.location.href ){
+
+}else{
+    locationHref.push(window.location.href);
+}
+let backAt = JSON.parse(localStorage.getItem("backAt")) || locationHref.length - 1;
+
+
+localStorage.setItem("locationHref", JSON.stringify(locationHref));
+
+document.getElementById("leftArrow").addEventListener("click", goBack);
+
+document.getElementById("rightArrow").addEventListener("click", goForward);
+
+
+
+function goBack() {
+    console.log("PressBack")
+    if(backAt >= 1){
+        console.log(backAt)
+        backAt--;
+    location.href = locationHref[backAt];
+    
+    forwardAt = backAt + 1;
+    }
+    console.log(backAt)
+
+    localStorage.setItem("forwardAt", JSON.stringify(forwardAt));
+    localStorage.setItem("backAt", JSON.stringify(backAt));
+}
+
+function goForward() {
+    console.log("PressForward")
+
+    if (forwardAt && locationHref[forwardAt]) {
+        location.href = locationHref[forwardAt];
+        if (locationHref[forwardAt++]) {
+            forwardAt++;
+            backAt++;
+        }
+        localStorage.setItem("forwardAt", JSON.stringify(forwardAt));
+        localStorage.setItem("backAt", JSON.stringify(backAt))
+    }
+
+}
